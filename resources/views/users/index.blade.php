d60fa74140e94023255cc00deee1f81f1c60d079
njunior <njunior@poential.com.br>
2019-05-09 14:52:35 -0300

    primeiras adequeacoes

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,39 +4,43 @@
 @endsection
 
 @if(!empty($mensagem))
-<div class="alert alert-success">
-    {{$mensagem}}
-</div>
+    <div class="alert alert-success">
+        {{$mensagem}}
+    </div>
 @endif
 
 <div class="container">
-<table class="table">
-    <thead class="thead-light">
-    <tr>
-        <th scope="col">Nome</th>
-        <th scope="col">Usuário</th>
-        <th scope="col">E-mail</th>
-        <th scope="col">Opções</th>
-    </tr>
-    </thead>
-    <tbody>
-
-    @foreach($users as $user)
-
+    <table class="table">
+        <thead class="thead-light">
         <tr>
-            <th scope="row">{{ $user->nome }}</th>
-            <td>{{ $user->usuario }}</td>
-            <td>{{ $user->email }}</td>
-            @csrf
-            <td><a href="/edit/{{ $user->id }}" class="btn btn-light btn-sm"><i class="fas fa-pencil-alt"></i></i> Editar</a>
-                <a href="/remove/{{ $user->id }}" class="btn btn-danger btn-sm"><i class="fas fa-times"></i> Deletar</a></td>
+            <th scope="col">Nome</th>
+            <th scope="col">Usuário</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Opções</th>
         </tr>
+        </thead>
+        <tbody>
 
-    @endforeach
+        @foreach($users as $user)
 
-    </tbody>
-</table>
+            <tr>
+                <th scope="row">{{ $user->nome }}</th>
+                <td>{{ $user->usuario }}</td>
+                <td>{{ $user->email }}</td>
+                @csrf
+                <td><a href="/edit/{{ $user->id }}" class="btn btn-light btn-sm"><i class="fas fa-pencil-alt"></i></i> Editar</a>
+                    <a href="/remove/{{ $user->id }}" class="btn btn-danger btn-sm"><i class="fas fa-times"></i> Deletar</a></td>
+            </tr>
+
+        @endforeach
+
+        </tbody>
+    </table>
+
+    <a href="/create" class="btn btn-primary mb-2 mt-1">Cadastrar</a>
 </div>
 
 </body>
-</html> 
+</html>
+
+
