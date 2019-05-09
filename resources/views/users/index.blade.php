13860900f19169f13598e55bf126436a4f2f079d
njunior <njunior@poential.com.br>
2019-05-09 17:21:06 -0300

    melhorias no frontend

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,7 +2,29 @@
 @section('cabecalho')
     Listagem de usuários
 @endsection
+<!-- barra de navegacao superior -->
 
+<nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+        <form class="form-inline my-2 my-lg-0">
+            <span class="navbar-text text-white mr-md-2">
+                Nome
+            </span>
+            <input class="form-control mr-sm-3" type="search" aria-label="Search">
+
+            <span class="navbar-text text-white mr-md-2">
+                E-mail
+            </span>
+            <input class="form-control mr-sm-3" type="search" aria-label="Search">
+
+            <button class="btn btn-info my-2 my-sm-0" type="submit">Filtrar</button>
+        </form>
+    </div>
+</nav>
+
+<br>
+
+
+<!-- IF para verificacao e alerta de sucesso -->
 @if(!empty($mensagem))
 
     <div class="alert alert-success" role="alert">
@@ -26,12 +48,12 @@
         @foreach($users as $user)
 
             <tr >
-                <td class="align-middle" scope="row">{{ $user->nome }}</    td>
+                <td class="align-middle" scope="row">{{ $user->nome }}</td>
                 <td class="align-middle">{{ $user->usuario }}</td>
                 <td class="align-middle">{{ $user->email }}</td>
                 @csrf
                 <td><a href="/edit/{{ $user->id }}" class="btn btn-light btn-sm"><i class="fas fa-pencil-alt"></i></i> Editar</a>
-                    <a href="/remove/{{ $user->id }}" class="btn btn-danger btn-sm"><i class="fas fa-times"></i> Deletar</a></td>
+                    <a href="/remove/{{ $user->id }}" class="btn btn-danger btn-sm" ><i class="fas fa-times"></i> Deletar</a></td>
             </tr>
 
         @endforeach
