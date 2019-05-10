6b3ba26afcb9d559f290d69828af4f19e40d4179
narcisao <narcisoderp@gmail.com>
2019-05-09 22:58:57 -0300

    inicio da paginacao

--- resources/views/users/delete.blade.php
+++ resources/views/users/delete.blade.php
@@ -60,31 +60,31 @@
 
     </div>
 
-<!--
-<div id="delete" class="modal fade text-danger" role="dialog">
-   <div class="modal-dialog ">
-     <form action="" id="deleteForm" method="post">
-         <div class="modal-content">
-             <div class="modal-header bg-danger">
-                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                 <h4 class="modal-title text-center">DELETE CONFIRMATION</h4>
-             </div>
-             <div class="modal-body">
-                 {{ csrf_field() }}
-                 {{ method_field('DELETE') }}
-                 <p class="text-center">Are You Sure Want To Delete ?</p>
-             </div>
-             <div class="modal-footer">
-                 <center>
-                     <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
-                     <button type="submit" name="" class="btn btn-danger" data-dismiss="modal" onclick="formSubmit()">Yes, Delete</button>
-                 </center>
-             </div>
-         </div>
-     </form>
-   </div>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
   </div>
-  -->
+</div>
 
 
 </body>
