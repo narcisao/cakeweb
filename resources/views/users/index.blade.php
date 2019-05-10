07ca84ffa44b2f50eb1e88ea57e5fadcef7f6528
narcisao <narcisoderp@gmail.com>
2019-05-09 23:14:16 -0300

    otimizacao da pagination

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -59,28 +59,15 @@
         </tbody>
     </table>
 
-    {{ $users->links() }}
+    <ul class="pagination justify-content-center">
+        <a href="/create" class="btn btn-primary mb-2 mt-1">Cadastrar</a>
+    </ul>
 
     <ul class="pagination justify-content-center">
-    <a href="/create" class="btn btn-primary mb-2 mt-1">Cadastrar</a>
+        {{ $users->links() }}
     </ul>
-    
+
 </div>
-
-<nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
-
 </body>
 </html>
 
